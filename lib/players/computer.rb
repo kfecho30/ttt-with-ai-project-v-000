3efdc6b3a575be679dd860b633074d4a2a4ac659
Kelsey Fecho <kfecho30@gmail.com>
2018-03-18 22:46:24 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ require_relative '../player.rb'
 module Players
   class Computer < Player
     def move(board)
-      board.update(rand(1..9), self.token)
+      board.update(rand(1..9).to_i, self.token)
     end
   end
 end
