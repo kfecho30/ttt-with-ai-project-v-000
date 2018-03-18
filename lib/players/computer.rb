852bcdb0d74fa3b87d2fb694b337b6480b21420a
Kelsey Fecho <kfecho30@gmail.com>
2018-03-18 23:21:49 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,6 +5,14 @@ module Players
     def move(board)
       if board.valid_move?(5)
         "5"
+      elsif board.valid_move?(1)
+        "1"
+      elsif board.valid_move?(7)
+        "7"
+      elsif board.valid_move?(9)
+        "9"
+      elsif board.valid_move?(3)
+        "3"
       else
         rand(1..9).to_s
       end
