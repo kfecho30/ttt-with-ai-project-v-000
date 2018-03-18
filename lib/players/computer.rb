a830688dc50f98a487756fe791184c783d6df18a
Kelsey Fecho <kfecho30@gmail.com>
2018-03-18 23:09:58 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,11 @@ require_relative '../player.rb'
 module Players
   class Computer < Player
     def move(board)
-      rand(1..9).to_s
+      if board.valid_move?(5)
+        "5"
+      else
+        rand(1..9).to_s
+      end
     end
   end
 end
